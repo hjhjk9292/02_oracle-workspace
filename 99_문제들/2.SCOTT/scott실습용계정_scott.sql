816752895a2f89230c239b4f5a9911f75e32b9ff
Hyeonji Kim <hjhjk9292@gmail.com>
2024-12-29 23:02:03 +0900

    scott practice

--- 99_문제들/2.SCOTT/scott실습용계정_scott.sql
+++ 99_문제들/2.SCOTT/scott실습용계정_scott.sql
@@ -22,7 +22,7 @@ ORDER BY SAL DESC;
 ----5. EMP테이블에서 급여가 같을 경우 커미션을 내림차순 정렬 조회 
 SELECT *
 FROM EMP
-ORDER BY COMM DESC;
+ORDER BY SAL DESC, COMM DESC;
 
 --6. EMP테이블에서 사원번호, 사원명, 직급, 입사일 조회 (단, 입사일을 오름차순 정렬 처리) 
 SELECT EMPNO, ENAME, JOB, HIREDATE
@@ -47,14 +47,15 @@ SELECT SYSDATE FROM DUAL;
 
 ----10. EMP테이블에서 사번, 사원명, 급여 조회  
 --(단, 급여는 100단위까지의 값만 출력 처리하고 급여 기준 내림차순 정렬) -- 1750이면 50 안나오게 반올림
-SELECT EMPNO, ENAME, SUBSTR(SAL, 1, 3)
+-- ROUND(SAL, -2)는 숫자를 가장 가까운 100 단위로 반올림하는 역할
+SELECT EMPNO, ENAME, ROUND(SAL, -2) 
 FROM EMP
 ORDER BY SAL DESC;
 
-----11. EMP테이블에서 사원번호가 홀수인 사원들을 조회 
+----11. EMP테이블에서 사원번호가 홀수인 사원들을 조회 (MOD 함수는 나머지 연산을 수행하는 함수)
 SELECT *
 FROM EMP
-WHERE SUBSTR(EMPNO, 4 ,1) IN ('1', '3', '5', '7', '9');
+WHERE MOD(EMPNO, 2) = 1;
 
 ----12. EMP테이블에서 사원명, 입사일 조회 (단, 입사일은 년도와 월을 분리 추출해서 출력)
 SELECT ENAME,
@@ -66,21 +67,28 @@ FROM EMP;
 ----13. EMP테이블에서 9월에 입사한 직원의 정보 조회 
 SELECT ENAME
 FROM EMP
-WHERE EXTRACT(MONTH FROM HIREDATE) AS "입사월";
+WHERE EXTRACT(MONTH FROM HIREDATE) = 9;
 
 --14. EMP테이블에서 81년도에 입사한 직원 조회 
-
+SELECT *
+FROM EMP
+WHERE EXTRACT(YEAR FROM HIREDATE) = 1981;
 
 --15. EMP테이블에서 이름이 'E'로 끝나는 직원 조회 
-
+SELECT *
+FROM EMP
+WHERE ENAME LIKE '%E';
 
 --16. EMP테이블에서 이름의 세 번째 글자가 'R'인 직원의 정보 조회 
-
 --16-1. LIKE 사용 
-
+SELECT *
+FROM EMP
+WHERE ENAME LIKE '__R%';
 
 --16-2. SUBSTR() 함수 사용 
-
+SELECT *
+FROM EMP
+WHERE SUBSTR(ENAME, 3, 1) = 'R';
 
 ----17. EMP테이블에서 사번, 사원명, 입사일, 입사일로부터 40년 되는 날짜 조회
 SELECT ENAME, HIREDATE, FLOOR(SYSDATE - HIREDATE) || '일'
